e671f93e51fba3c9c2535ca6bb7ceaee0e3edc0e
Bonaventure Anselm <bona.anselm@gmail.com>
2023-06-27 10:32:10 +0100

    Updated syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,11 @@
 #!/bin/usr/node
 
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
