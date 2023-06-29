f355d66c3f51fff220f77d77a5bd0d68a8f751a6
Bonaventure Anselm <bona.anselm@gmail.com>
2023-06-29 06:44:01 +0100

    Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise
+    .allSettled([userPromise, photoPromise])
+    .then((response) => (
+      response.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+      }))
+    ));
+}
