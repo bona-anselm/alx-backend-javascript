996e425196ac01e265a6c509b227aa6cd56a9f67
Bonaventure Anselm <bona.anselm@gmail.com>
2023-06-29 07:17:50 +0100

    Task 100

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let response = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    response = { photo, user };
+  } catch (error) {
+    response = { photo: null, user: null };
+  }
+  return response;
+}
