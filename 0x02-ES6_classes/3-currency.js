fb2c3730f0ec764a6bd3f04b6edc1264e37ad18e
Bonaventure Anselm <bona.anselm@gmail.com>
2023-06-30 08:26:31 +0100

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(value) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = value;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    if (typeof _name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
