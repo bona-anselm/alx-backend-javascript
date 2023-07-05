89545e6327ead86232d9880cdb4e2f8d98f4b874
Bonaventure Anselm <bona.anselm@gmail.com>
2023-07-05 05:23:18 +0100

    Task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+export default function getStudentsByLocation(students, city) {
+  if (!(students instanceof Array)) {
+    return [];
+  }
+  return students.filter((student) => student.location === city);
+}
