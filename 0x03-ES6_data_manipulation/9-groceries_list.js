8c077dc6c8d53932c83253b9340dab880f0b7b77
Bonaventure Anselm <bona.anselm@gmail.com>
2023-07-05 05:57:29 +0100

    Task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
