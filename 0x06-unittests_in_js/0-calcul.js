d9ee6185dfdd8e18b9d23dd078f7a844b007c803
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-25 05:50:14 +0100

    Task 0

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+const calculateNumber = (a, b) {
+  return Math.round(Number(a)) + Math.round(Number(b));
+};
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,3 @@
+# Unittests in JS
+
+To be updated...
