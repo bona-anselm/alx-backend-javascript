64000c8e440d8e15b71e6a7eb309250f6a88ea20
Bonaventure Anselm <bona.anselm@gmail.com>
2023-08-25 07:50:12 +0100

    Task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,75 +1,63 @@
 const express = require('express');
-const fs = require('fs');
+
+const { readFile } = require('fs');
 
 const app = express();
-const hostname = 'http://127.0.0.1';
 const port = 1245;
 
-const countStudents = (filePath) => new Promise((resolve, reject) => {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (data) {
-      const linesOfFile = data
-        .toString('utf-8')
-        .trim()
-        .split('\n');
-      const studentBatch = {};
-      const fieldNames = linesOfFile[0].split(',');
-      const studentPropNames = fieldNames
-        .slice(0, fieldNames.length - 1);
-      for (const line of linesOfFile.slice(1)) {
-        const studentRecord = line.split(',');
-        const studentPropValues = studentRecord
-          .slice(0, studentRecord.length - 1);
-        const field = studentRecord[studentRecord.length - 1];
-        if (!Object.keys(studentBatch).includes(field)) {
-          studentBatch[field] = [];
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
         }
-        const studentEntries = studentPropNames
-          .map((propName, idx) => [propName, studentPropValues[idx]]);
-        studentBatch[field].push(Object.fromEntries(studentEntries));
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
+    });
+  });
+}
 
-      const totalStudents = Object
-        .values(studentBatch)
-        .reduce((pre, cur) => (pre || []).length + cur.length);
-
-      // Create a formatted output string
-      let output = `Number of students: ${totalStudents}\n`;
-      for (const [field, group] of Object.entries(studentBatch)) {
-        const studentNames = group.map((student) => student.firstname).join(', ');
-        output += `Number of students in ${field}: ${group.length}. List: ${studentNames}\n`;
-      }
-
-      resolve(output);
-    }
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+app.get('/students', (request, response) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    response.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    response.send('This is the list of our students\nCannot load the database');
   });
 });
 
-// Define a route for the root path
-app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
-});
-
-// Define a route for the "/students" path
-app.get('/students', (req, res) => {
-  countStudents(process.argv[2].toString())
-    .then((output) => {
-      res.status(200).send(`This is the list of our students\n${output}`);
-    })
-    .catch(() => {
-      res.status(404).send('Cannot load the database');
-    });
-});
-
-// Define a catch-all route for 404 errors
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
-
-// Start the Express.js application
 app.listen(port, () => {
-  console.log(`Server running at ${hostname}:${port}/`);
 });
+
+module.exports = app;
